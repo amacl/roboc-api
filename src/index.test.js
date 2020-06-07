b9775301bb8f6dc259bcdf6bb75e756de22a6092
Pablo Rebolledo Acosta <pablo.rebolledo.acosta@gmail.com>
2020-06-06 22:00:14 -0400

    [imp] add improvements for alive and router

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,50 @@
-const request = require('supertest')
-import api from '../src/index'
+const request = require('supertest');
+import api from '../src/index';
 
 describe('GET /alive', function() {
-    it('responds with json', function(done) {
+    it('responds with json by string alive', function(done) {
         request(
             api({
                 prefix: '/',        
                 port: 123,
                 host: '0.0.0.0',
                 alive: '/alive',
-            },(router) => {
-                return router
+            },(router, app) => {
+                return router;
+            })              
+        ).get('/alive')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)        
+        .expect(200,{
+            "OK": true,
+            "status": 200,
+            "message": "alive"
+        },done);
+    });
+
+    it('responds with json by array alive', function(done) {
+        request(
+            api({
+                prefix: '/',        
+                port: 124,
+                host: '0.0.0.0',
+                alive: ['/alive', (req, res, next) => {
+                    res.status(200).json({
+                        OK:true,
+                        status:200,
+                        message:"alive-array",
+                    }); 
+                }],
+            },(router, app) => {
+                return router;
             })              
         ).get('/alive')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
-        .expect(200, done)
-    })
-  })
+        .expect(200,{
+            "OK": true,
+            "status": 200,
+            "message": "alive-array"
+        },done);  
+    })        
+});
